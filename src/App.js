fc9eeb4a17685dfd0a8d2a93d5fda705c0338c89
youjeahyun <wogusdl9747@naver.com>
2023-03-02 18:05:51 +0900

    route

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import './App.css';
 import Day from './componenet/Day';
 import DayList from './componenet/DayList';
 import Header from './componenet/Header';
+import { BrowserRouter, Route , Routes } from 'react-router-dom';
+import EmptyPage from './componenet/EmptyPage';
 
 function App() {
 
-  return (
-    <div className="App">     
-      <Header/>
-      <DayList/>
-      <Day />
+  return ( 
+    <div className='App'>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<DayList/>} />
+        <Route path="/day/:day" element={<Day/>} />
+        <Route path="/day/4" element={<EmptyPage/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
-  );
+);
 }
 
 export default App;
--- src/componenet/Day.js
+++ src/componenet/Day.js
@@ -1,21 +1,20 @@
+import { useParams } from "react-router-dom";
 import dummy from "../db/data.json"
 export default function Day() {
-    const day =2 ;
-    const wordList = dummy.words.filter(word=> word.day === day);
+    const {day} = useParams();
+    const wordList = dummy.words.filter(word=> word.day === Number(day));
     //console.log(wordList);
   
+   
     return (
      <>
+        <h2>Day {day}</h2>
         <table>
             <tbody>
                 {wordList.map(word=>(
                     <tr key={word.id}>
-                        <td>
-                            {word.eng}
-                        </td>
-                        <td>
-                            {word.kor}
-                        </td>
+                        <td>{word.eng}</td>
+                        <td>{word.kor}</td>
                     </tr>
                 ))}
             </tbody>
